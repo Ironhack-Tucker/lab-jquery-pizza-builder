218fcaced7fa8f3ce03b41704c9eadf0edd449fe
Xenock <miguelmoracho@gmail.com>
2017-04-11 16:53:05 +0200

    Some iterations

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,35 @@
-// Write your Pizza Builder JavaScript in this file.
+function toggling(ingredient, button) {
+  button.on('click', function(){
+    ingredient.toggle()
+    button.toggleClass("active")
+  })
+}
+
+$(document).ready(function() {
+
+  var btnPepperoni = $(".btn-pepperonni")
+  var btnMushrooms = $(".btn-mushrooms")
+  var btnGreenPeppers = $(".btn-green-peppers")
+  var btnSauce = $(".btn-sauce")
+  var btnCrust =$(".btn-crust")
+
+  var greenPeppers = $(".green-pepper")
+  var mushroom = $(".mushroom")
+  var pepperoni = $(".pep")
+  var sauce = $(".sauce-white")
+  var crust = $(".crust")
+
+  toggling(pepperoni,btnPepperoni )
+  toggling(mushroom,btnMushrooms )
+  toggling(greenPeppers,btnGreenPeppers )
+  toggling(sauce,btnSauce )
+
+  crust.removeClass('crust-gluten-free')
+
+  btnCrust.on('click', function(){
+    console.log("hola")
+    crust.toggleClass("crust-gluten-free")
+  })
+
+
+})
